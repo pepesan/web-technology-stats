20c997ebb71f8d0b9fccc72c94c1c2b04b4a7de0
pepesan <pepesan@gmail.com>
2019-12-01 14:26:10 +0100

    mejoras en la generación de gráficas, incluido logo como marca de agua,

--- genera.py
+++ genera.py
@@ -84,26 +84,84 @@ def printAllDF(df):
         print(df)
 
 
-def generateFigurePlotly(num, df, tamano, criterio):
+def generateFigurePlotly(num, df, tamano, criterio, formats):
     colores = ["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43",
                "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
-               "#fdae61", "#f46d43"]
+               "#fdae61", "#f46d43", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43"
+               ]
     #print("DF Filtrado "+ str(df.head()))
+    layout = go.Layout(
+        autosize=False,
+        width=2000,
+        height=2000
+    )
     fig = go.Figure(data=[go.Bar(
         x=df.head(tamano)['nombre'], y=df.head(tamano)['porcentajes'],
         text=df.head(tamano)['porcentajes'],
         marker_color=colores,
         textposition='auto',
-    )])
+    )], layout=layout)
+    import base64
+    with open("./data/logo.png", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
+    # add the prefix that plotly will want when using the string as source
+    encoded_image = "data:image/png;base64," + encoded_string
+    fig.add_layout_image(
+        go.layout.Image(
+            #source="http://cursosdedesarrollo.com/wp-content/uploads/2019/12/logo-cuadrado.png",
+            source=encoded_image,
+            #xref="x",
+            #yref="y",
+            xref="paper",
+            yref="paper",
+            x=0,
+            y=1,
+            sizex=1,
+            sizey=1,
+            sizing="stretch",
+            opacity=0.1,
+            layer="above")
+    )
     fig.update_layout(
         title={
             'text': "Porcentaje de Uso de Tecnologías para " + str(num) + " sitios web: "+criterio['name'],
             'y': 0.9,
             'x': 0.5,
             'xanchor': 'center',
-            'yanchor': 'top'})
+            'yanchor': 'top'},
+        template="plotly_white")
     # fig.show()
-    fig.write_image("report/barras_plotly_" + str(num) + "_" + str(tamano) + "_"+str(criterio['name'])+".png")
+    for f in formats:
+        fig.write_image(
+            "report/barras_plotly_" + str(num) + "_" + str(tamano) + "_"+str(criterio['name'])+"."+f
+        )
 
 
 def generateFigureBokeh(num, df, tamano):
@@ -114,6 +172,8 @@ def generateFigureBokeh(num, df, tamano):
     output_file("report/barras_bokeh_" + str(num) + ".html")
 
     colores = ["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43",
+               "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
+               "#fdae61", "#f46d43","#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43",
                "#d53e4f", "#9e0142", "#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b",
                "#fdae61", "#f46d43"]
     source = ColumnDataSource(df.head(tamano))
@@ -128,7 +188,7 @@ def generateFigureBokeh(num, df, tamano):
     # export_png(p, filename="report/plot.png")
 
 
-def generateAndSavePlot(num, tamanos, criterio):
+def generateAndSavePlot(num, tamanos, criterio, formats):
     print("Número de Resultados: " + str(num))
     df = generate_sites(num, criterio)
     if (criterio['tech']!= None):
@@ -160,6 +220,7 @@ def generateAndSavePlot(num, tamanos, criterio):
     df['textos'] = textos
     index = 1
     for row in df.iterrows():
+        #print("Fila: "+ str(row))
         try:
             data = Resultado.objects().get(tech=row[1][0], total=num, batch=BATCH)
             # print(data)
@@ -170,8 +231,9 @@ def generateAndSavePlot(num, tamanos, criterio):
         resultado = Resultado()
         resultado.tech = row[1][0]
         resultado.total = num
-        resultado.resultDecimal = row[1][3]
+        resultado.resultDecimal = row[1][1]
         resultado.resultString = row[1][2]
+        resultado.resultPercentage= row[1][3]
         resultado.finished = True
         resultado.position = index
         resultado.batch = BATCH
@@ -183,17 +245,18 @@ def generateAndSavePlot(num, tamanos, criterio):
             print(e)
 
     for tamano in tamanos:
-        print(df.head(tamano))
-        generateFigurePlotly(num, df, tamano, criterio)
+        #print(df.head(tamano))
+        generateFigurePlotly(num, df, tamano, criterio, formats)
         # generateFigureBokeh(num, df, tamano)
 
 
 if (os.path.isdir(REPORTDIR) == False):
     os.mkdir(REPORTDIR)
 
-nums = [10, 50, 100, 500, 1000, 10000, 100000, 200000, 300000, 400000, 500000, 600000]
+nums = [10, 50, 100, 500, 1000, 10000, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000]
 #nums = [100]
-tamanos = [10, 20]
+tamanos = [10, 20, 30, 40, 100]
+formats=['png', 'svg']
 criterios = [
     {
         'name': 'Todos',
@@ -201,42 +264,84 @@ criterios = [
         'tech': None
     },
     {
-        'name': 'CMSs_PHP',
+        'name': 'Tiendas-Online',
+        'query': {
+                 '$or': [
+                    {'tech': 'Magento'},
+                    {'tech': 'WooCommerce'},
+                    {'tech': 'PrestaShop'},
+                    {'tech': 'Wix'},
+                    {'tech': "Shopify"}
+                  ]
+        },
+        'tech': ["Magento", "WooCommerce", "PrestaShop", "Wix", "Shopify"]
+    },
+    {
+        'name': 'Sistemas-Operativos',
+        'query': {
+                 '$or': [
+                    {'tech': 'Ubuntu'},
+                    {'tech': 'Debian'},
+                    {'tech': 'CentOS'},
+                    {'tech': 'Fedora'},
+                    {'tech': "Red Hat"},
+                    {'tech': "UNIX"},
+                    {'tech': "Windows Server"}
+                  ]
+        },
+        'tech': ["Ubuntu", "Debian", "CentOS", "Fedora", "Red Hat", "UNIX", "Windows Server"]
+    },
+    {
+        'name': 'CMSs',
         'query': {
                  '$or': [
                     {'tech': 'WordPress'},
                     {'tech': 'Drupal'},
-                    {'tech': 'Joomla'}
+                    {'tech': 'Joomla'},
+                     {'tech': "Liferay"},
+                     {'tech': "Blogger"},
+                     {'tech': "OpenGSE"},
+                     {'tech': "1C-Bitrix"}
                   ]
         },
-        'tech': ["WordPress", "Drupal", "Joomla"]
+        'tech': ["WordPress", "Drupal", "Joomla", "Liferay", "Blogger", "OpenGSE", "1C-Bitrix"]
     },
     {
-        'name': 'Servers',
+        'name': 'Servidores-Web',
         'query': {
                  '$or': [
                      {'tech': 'Apache'},
                      {'tech': 'IIS'},
+                     {'tech': "IIS\\\\;confidence:50"},
                      {'tech': 'Nginx'},
                      {'tech': 'Tengine'},
-                     {'tech': "Apache Tomcat"}
+                     {'tech': "LiteSpeed"}
                   ]
         },
-        'tech': ["Apache", "IIS", "Nginx", "Tengine", "Apache Tomcat"]
+        'tech': ["Apache", "IIS", "IIS\\\\;confidence:50", "Nginx", "Tengine", "Apache Tomcat", "LiteSpeed"]
     },
     {
-        'name': 'Servers',
+        'name': 'BackendServerAppContainer',
         'query': {
                  '$or': [
                      {'tech': 'Java'},
+                     {'tech': "Apache Tomcat"},
                      {'tech': 'PHP'},
                      {'tech': 'Python'},
                      {'tech': 'Ruby on Rails'},
                      {'tech': "Microsoft ASP.NET"},
-                     {'tech': "Java Servlet"}
+                     {'tech': "Java Servlet"},
+                     {'tech': 'Ruby'},
+                     {'tech': 'Node.js'},
+                     {'tech': "Express"},
+                     {'tech': 'Ruby on Rails'},
+                     {'tech': "Lua"},
+                     {'tech': "OpenResty"}
                   ]
         },
-        'tech': ["Java", "PHP", "Python", "Ruby on Rails", "Microsoft ASP.NET", "Java Servlet"]
+        'tech': ["Apache Tomcat", "Java", "PHP", "Python", "Ruby on Rails", "Microsoft ASP.NET",
+                 "Java Servlet", 'Ruby', "Node.js", "Ruby on Rails", "Lua", "OpenResty",
+                 "Express"]
     },
     {
         'name': 'DataBases',
@@ -245,10 +350,43 @@ criterios = [
                      {'tech': "MySQL"},
                      {'tech': "PostgreSQL"},
                      {'tech': "MariaDB"},
-                     {'tech': "MongoDB"}
+                     {'tech': "MongoDB"},
+                     {'tech': "Percona"}
                   ]
         },
-        'tech': ["MySQL", "PostgreSQL", "MariaDB", "MongoDB"]
+        'tech': ["MySQL", "PostgreSQL", "MariaDB", "MongoDB", "Percona"]
+    },
+    {
+        'name': 'RRSS',
+        'query': {
+                 '$or': [
+                     {'tech': "Facebook"},
+                     {'tech': "Twitter"},
+                     {'tech': "Pinterest"},
+                     {'tech': "Linkedin"},
+                     {'tech': "YouTube"},
+                     {'tech': "Google Plus"}
+                  ]
+        },
+        'tech': ["Facebook", "Twitter", "Pinterest", "Linkedin", "YouTube", "Google Plus"]
+    },
+{
+        'name': 'WP-Plugins',
+        'query': {
+                 '$or': [
+                     {'tech': 'Yoast SEO'},
+                     {'tech': 'WordPress Super Cache'},
+                     {'tech': 'Elementor'},
+                     {'tech': "All in One SEO Pack"},
+                     {'tech': "AddThis"},
+                     {'tech': "W3 Total Cache"},
+                     {'tech': "WP Rocket"},
+                     {'tech': "Gravity Forms"},
+                     {'tech': 'AMP Plugin'}
+                  ]
+        },
+        'tech': ['Yoast SEO', 'WordPress Super Cache', 'Elementor', "All in One SEO Pack", "AddThis",
+                 "W3 Total Cache", "WP Rocket", "Gravity Forms", 'AMP Plugin']
     },
     {
         'name': 'JSFrameWorks',
@@ -261,17 +399,117 @@ criterios = [
                      {'tech': "jQuery UI"},
                      {'tech': "jQuery Mobile"},
                      {'tech': "Vue"},
-                     {'tech': "Vue.js"}
+                     {'tech': "Vue.js"},
+                     {'tech': "MooTools"},
+                     {'tech': "Backbone.js"}
                   ]
         },
-        'tech': ["Angular", "Angular JS", "React","jQuery", "jQuery UI", "jQuery Mobile", "Vue", "Vue.js"]
+        'tech': ["Angular", "Angular JS", "React", "jQuery", "jQuery UI", "jQuery Mobile", "Vue", "Vue.js",
+                 "MooTools", "Backbone.js"]
+    },
+    {
+        'name': 'VariousTech',
+        'query': {
+                 '$or': [
+                     {'tech': "Font Awesome"},
+                     {'tech': "Material Design Lite"},
+                     {'tech': "Bootstrap"},
+                     {'tech': "ZURB Foundation"},
+                     {'tech': "jQuery UI"},
+                     {'tech': "jQuery Mobile"},
+                     {'tech': "Ionic"},
+                     {'tech': "UIKit"},
+                     {'tech': "Materialize CSS"},
+                     {'tech': "animate.css"},
+                     {'tech': "Lightbox"},
+                     {'tech': 'DataTables'},
+                     {'tech': "OWL Carousel"},
+                     {'tech': 'Modernizr'},
+                     {'tech': 'CloudFlare'},
+                     {'tech': 'reCAPTCHA'},
+                     {'tech': "Sucuri"},
+                     {'tech': "Akamai"},
+                     {'tech': 'Varnish'},
+                     {'tech': "Slick"},
+                     {'tech': "Revslider"},
+                     {'tech': "OpenSSL"},
+                     {'tech': "Underscore.js"},
+                     {'tech': "FancyBox"},
+                     {'tech': "Liveinternet"},
+                     {'tech': "Plesk"},
+                     {'tech': "Swiper Slider"},
+                     {'tech': "MailChimp"},
+                     {'tech': "Twitter"},
+                     {'tech': "prettyPhoto"},
+                     {'tech': "RequireJS"},
+                     {'tech': "Gravatar"},
+                     {'tech': "Moment.js"},
+                     {'tech': "Litespeed Cache"},
+                     {'tech': "Polyfill"},
+                     {'tech': "Handlebars"},
+                     {'tech': "TrackJs"},
+                     {'tech': "SWFObject"},
+                     {'tech': "Select2"},
+                     {'tech': "SiteGround"},
+                     {'tech': "SquareSpace"},
+                     {'tech': "FlexSlider"},
+                     {'tech': "comScore"},
+                     {'tech': "Clipboard.js"},
+                     {'tech': "TweenMax"},
+                     {'tech': "Prototype"},
+                     {'tech': "Lazy.js"},
+                     {'tech': "Ionicons"},
+                     {'tech': "Chart.js"}
+                  ]
+        },
+        'tech': ["Font Awesome", "Material Design Lite", "Bootstrap", "ZURB Foundation", "jQuery UI",
+                 "jQuery Mobile", "Ionic", "UIKit", "Materialize CSS", "animate.css", "Lightbox",
+                 'DataTables', "OWL Carousel", "TrackJs", "SWFObject", "Select2", "SiteGround"
+                 'Modernizr', 'CloudFlare', 'reCAPTCHA', "Sucuri", "SquareSpace", "FlexSlider"
+                 "Akamai", 'Varnish', "Slick", "Revslider", "OpenSSL", "Underscore.js", "FancyBox",
+                 "Liveinternet",  "Swiper Slider",  "Plesk",  "MailChimp", "Twitter", "prettyPhoto",
+                 "RequireJS", "Gravatar", "Moment.js", "Litespeed Cache", "Polyfill", "Handlebars",
+                 "comScore", "Clipboard.js", "TweenMax", "Prototype", "Lazy.js", "Ionicons", "Chart.js"]
+    },
+    {
+        'name': 'Amazon-Tech',
+        'query': {
+                 '$or': [
+                     {'tech': "Amazon Web Services"},
+                     {'tech': "Amazon CloudFront"},
+                     {'tech': 'Amazon S3'},
+                     {'tech': 'Amazon EC2'}
+                  ]
+        },
+        'tech': ["Amazon Web Services", "Amazon CloudFront", 'Amazon S3', 'Amazon EC2']
+    },
+    {
+        'name': 'Google',
+        'query': {
+                 '$or': [
+                     {'tech': "Google Tag Manager"},
+                     {'tech': "Google AdSense"},
+                     {'tech': "YouTube"},
+                     {'tech': "reCAPTCHA"},
+                     {'tech': "Google Plus"},
+                     {'tech': "Google Font API"},
+                     {'tech': 'Google Analytics'},
+                     {'tech': "Google Maps"},
+                     {'tech': "Google PageSpeed"},
+                     {'tech': "DoubleClick for Publishers (DFP)"}
+                  ]
+        },
+        'tech': ["Google Tag Manager", "Google AdSense", "YouTube", "reCAPTCHA", "Google Plus",
+                 "Google Font API", "Google Analytics", "Google Maps", "Google PageSpeed",
+                 "DoubleClick for Publishers (DFP)"]
     }
 ]
 
 
 for criterio in criterios:
+    print("Criterio: " + str(criterio['name']))
     for num in nums:
-        generateAndSavePlot(num, tamanos, criterio)
+        generateAndSavePlot(num, tamanos, criterio, formats)
 
 
 #generateAndSavePlot(10000, tamanos, criterios[1])
--- gestionaDatos.py
+++ gestionaDatos.py
@@ -1,7 +1,19 @@
 from datetime import datetime
 from mongoengine import *
 
-connect(host='127.0.0.1', port=27017, db='test')
+connected = False
+try:
+    connect(host='127.0.0.1', port=27017, db='test')
+    connected = True
+except Exception as e:
+    print("No se puede conectar a la BBDD por lo que se finaliza la ejecución")
+    print("No olvides arrancar la BBDD MongoDB, antes de ejecutar los scripts")
+    print("Motivo: " + str(e))
+
+if(connected == True):
+    print("Se ha conectado al servidor")
+else:
+    exit(0)
 
 
 class Site(DynamicDocument):
